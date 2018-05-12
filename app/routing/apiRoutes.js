f78d522efaa9a4fb76e0415a2ccb2b781b2c2eac
unknown <mdibble93@gmail.com>
2018-05-12 04:07:42 -0400

    added to apiRoutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,7 +7,35 @@ module.exports = function(app) {
         res.json(friends);
     });
 
+    // POST handles the incoming survey results and is used to handle the compatibility logic
     app.post("/api/friends", jsonParser, function(req, res){
         friends.push(req.body);
+
+        // var compatibleFriend = compatibility(request, response);
+        // response.json(compatibility);
+        
     });
+
+};
+
+function userValidation() {
 }
+
+function compatibility(req, res) {
+    var newFriend = {
+        name: "",
+        photo: "",
+        scoreDifference: 1000
+    }
+
+    var userAnswers = req.body;
+    var userScores = userAnswers.scores;
+
+    var totalDifference = 1000;
+
+    // Loop through the "friends" array
+    for(var i = 0; i < friends.length; i++) {
+
+    }
+}
+
